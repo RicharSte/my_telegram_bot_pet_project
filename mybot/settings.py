db5ad1a3d38f0e7de3373e996aa92da0bb6d51f6
Stepabov Nikolai <Data.Science.NikSte@yandex.ru>
2020-09-30 21:17:05 +0300

    My bot in telegram

---
+++ mybot/settings.py
@@ -0,0 +1,9 @@
+PROXY = {
+    'proxy_url': 'socks5://t1.learn.python.ru:1080',
+    'urllib3_proxy_kwargs': {
+        'username': 'learn', 
+        'password': 'python'
+    }
+}
+
+BOT_TOKEN = 'Token from Bot father'
